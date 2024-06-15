dfe737157e4555709e3196346282b398ab74f0fe
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-06-14 22:19:28 -0700

    :/

--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -10,7 +10,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useRoute } from 'vue-router'
 import { ref } from 'vue'
 import { Pie } from 'vue-chartjs'
@@ -18,46 +18,59 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 ChartJS.register(ArcElement, Tooltip, Legend)
 
 const route = useRoute()
-const licenses = ref({})
-let colours = []
+
+// Define interface for project data
+interface Project {
+  license: {
+    id: string
+  }
+  color: number // Assuming color is a number
+}
+
+// Define interface for licenses object
+interface LicenseCounts {
+  [key: string]: number // Key is license ID, value is count
+}
+
+const licenses = ref<LicenseCounts>({}) // Initialize as empty object
+const colours = ref<string[]>([]) // Array of hex color codes
 const loaded = ref(false)
+
 console.log(route.params.user)
 
 const response = await fetch(`https://api.modrinth.com/v2/user/${route.params.user}/projects`)
-const data = await response.json()
-//console.log(data)
+const data: Project[] = await response.json() // Assume response is an array of projects
 
-for (let i = 0; i < data.length; i++) {
-  //console.log(data[i].license.id)
-  if (!Object.prototype.hasOwnProperty.call(licenses.value, data[i].license.id)) {
-    //licenses.value.push(data[i].license.id, 1)
-    licenses.value[data[i].license.id] = 1
-    if (data[i].color.toString(16) != 'ffffff') {
-      colours.push('#' + data[i].color.toString(16))
+// Loop through projects with type checking
+for (const item of data) {
+  const projectId = item.license.id // Use const for clarity
+  if (!Object.prototype.hasOwnProperty.call(licenses.value, projectId)) {
+    licenses.value[projectId] = 1
+    if (item.color.toString(16) !== 'ffffff') {
+      colours.value.push('#' + item.color.toString(16))
     } else {
       const randomColour = '#' + Math.floor(Math.random() * 16777215).toString(16)
-      colours.push(randomColour)
+      colours.value.push(randomColour)
     }
   } else {
-    licenses.value[data[i].license.id]++
+    licenses.value[projectId]++
   }
 }
-//console.log(licenses.value)
-//console.log(colours)
 
 const options = {
   responsive: true,
   maintainAspectRatio: true
 }
+
 const chartData = {
   labels: Object.keys(licenses.value),
   datasets: [
     {
-      backgroundColor: colours,
+      backgroundColor: colours.value,
       data: Object.values(licenses.value)
     }
   ]
 }
+
 loaded.value = true
-//console.log(chartData)
 </script>
