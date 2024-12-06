596f17fcdae2f7fa5e8d3adf2f4f62a68ac5afdd
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-12-05 18:46:29 -0800

    currency conversions!

--- src/components/PriceNode.vue
+++ src/components/PriceNode.vue
@@ -4,6 +4,15 @@
     <div v-for="(interval, key) in data.prices.intervals" v-bind:key="interval">
       {{ capitaliseFirstLetter(key.toString()) }}:
       <span style="font-weight: bold"> {{ getProperCost(data.currency_code, interval) }}</span>
+      <span v-if="exchangeRate !== null">
+        ({{
+          getRenderedConversion(
+            requestedCurrency ?? 'CAD',
+            data.currency_code,
+            interval * (1 / (exchangeRate ?? 0))
+          )
+        }})
+      </span>
     </div>
     <span>Type: {{ capitaliseFirstLetter(data.prices.type) }}</span>
     <span>ID: {{ data.id }}</span>
@@ -15,7 +24,9 @@
 import cc from 'currency-codes'
 import currencyData from 'currency-data'
 const props = defineProps({
-  data: Object
+  data: Object,
+  exchangeRate: Number,
+  requestedCurrency: String
 })
 function capitaliseFirstLetter(string: String) {
   return string.charAt(0).toUpperCase() + string.slice(1)
@@ -26,5 +37,24 @@ function getProperCost(code: string, price: number) {
     currency: code
   })
   return format.format(price / Math.pow(10, format.resolvedOptions().maximumFractionDigits ?? 0))
+}
+function getRenderedConversion(code: string, code2: string, price: number) {
+  const format = new Intl.NumberFormat(currencyData.getCountriesByCurrencyISOCode(code)[0], {
+    style: 'currency',
+    currency: code2,
+    currencyDisplay: 'code'
+  })
+  const format2 = new Intl.NumberFormat(currencyData.getCountriesByCurrencyISOCode(code)[0], {
+    style: 'currency',
+    currency: code
+  })
+  return format2.format(
+    Number.parseFloat(
+      format
+        .format(price / Math.pow(10, format.resolvedOptions().maximumFractionDigits ?? 0))
+        .replace(/^0123456789.,/, '')
+        .replace(`${code2}`, '')
+    )
+  )
 }
 </script>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,10 +1,17 @@
 <template>
   <div v-if="loaded" class="statsContainer">
     <h1>modrinth product prices</h1>
+    use ?cur=CAD (or other preferred currency code) to access other conversions
     <div class="statsContainer" v-for="product in data" v-bind:key="product">
       <span>ID: {{ product.id }} &bull; Product Type: {{ product.metadata.type }}</span>
       <div class="payout_grid">
-        <PriceNode v-for="price in product.prices" :data="price" v-bind:key="price" />
+        <PriceNode
+          v-for="price in product.prices"
+          :exchangeRate="conversionData.rates[price.currency_code] ?? null"
+          :requestedCurrency="requestedCurrency"
+          :data="price"
+          v-bind:key="price"
+        />
       </div>
     </div>
   </div>
@@ -13,12 +20,32 @@
 <script setup lang="ts">
 import PriceNode from '@/components/PriceNode.vue'
 import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+let requestedCurrency = route.query.cur ?? ''
 
 const loaded = ref(false)
 const requestUrl = ref(`https://api.modrinth.com/_internal/billing/products`)
 
 const response = await fetch(requestUrl.value)
 const data = await response.json()
+
+const currencyResponse = await fetch('https://api.frankfurter.dev/v1/currencies')
+const currencyData = await currencyResponse.json()
+
+if (requestedCurrency === null) {
+  requestedCurrency = 'CAD'
+} else if (!Object.keys(currencyData).includes(requestedCurrency.toString().toUpperCase())) {
+  requestedCurrency = 'CAD'
+} else {
+  requestedCurrency = requestedCurrency.toString().toUpperCase()
+}
+
+const conversionResponse = await fetch(
+  `https://api.frankfurter.dev/v1/latest?base=${requestedCurrency}`
+)
+const conversionData = await conversionResponse.json()
 
 const USD = new Intl.NumberFormat('en-US', {
   style: 'currency',
